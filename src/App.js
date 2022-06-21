2f699650296585c38fc7e4d21a7a2d52f9ab283e
DESKTOP-0875TLU\issam <issamrobler@gmail.com>
2022-06-21 12:22:33 -0400

    replaced browserrouter with hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import {HashRouter as Router,Routes,Route} from 'react-router-dom'
 import { Nav } from './components/nav/Nav';
 import {Home} from './pages/home/Home'
 import {Breakfast} from './pages/breakfast/Breakfast'
